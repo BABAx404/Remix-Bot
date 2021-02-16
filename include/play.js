b299f124fdc59f054790c7f89e9cf065656686ec
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-16 18:14:52 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -167,7 +167,6 @@ module.exports = {
         .setAuthor(`✅ Play Now Music...`)
         .setImage(thumb)
         .setDescription(`[Support](https://discord.gg/6Gv895N5hB) | [Invite](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=8&scope=bot)
-       
 • ──────☔ ────── •`)
       
         .setTimestamp()
