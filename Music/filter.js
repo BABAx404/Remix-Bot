42021f64eba30b151c3f0b1aec9c1c10720a6f9a
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-16 14:54:02 +0300

    Update filter.js

--- Music/filter.js
+++ Music/filter.js
@@ -82,7 +82,7 @@ async execute(message, args, client) {
         varforfilter = 8;
         break;
       case "subboost":
-        varforfilter = 9;
+        varforfilter = 9; it
         break;
       case "clear":
       varforfilter = 10;
@@ -116,7 +116,7 @@ async execute(message, args, client) {
       const song = queue.songs[0];
       //play the collected song song, message, client, filters
       message.channel.send(new MessageEmbed()
-      .setColor("#c219d8")
+      .setColor("RANDOM")
       .setAuthor("Applying: " + args[0], "https://cdn.discordapp.com/emojis/769935094285860894.gif")).then(msg =>{
         msg.delete({timeout: 2000});
       })
