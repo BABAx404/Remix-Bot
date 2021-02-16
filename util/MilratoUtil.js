11392f53943e9db69fbf50bd6f1ba757de3a841c
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-16 14:45:22 +0300

    Update MilratoUtil.js

--- util/MilratoUtil.js
+++ util/MilratoUtil.js
@@ -10,7 +10,7 @@ module.exports = {
     //define the EMBED
     let resultsEmbed = new MessageEmbed()
       .setTitle("❗️ | You must be in the Same Voice Channel as me!")
-      .setColor("#ff0e7a")
+      .setColor("RANDOM")
     //wenn memberchannel nicht der botchannel ist
     if (member.voice.channel !== member.guild.me.voice.channel) {
     //Send the message to the MEMBER
