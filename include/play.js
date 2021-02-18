8c021632255b81406c45ccda55860d72f5e8bfaa
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-18 20:50:01 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -161,7 +161,7 @@ module.exports = {
 
     try {
       const newsong = new MessageEmbed()
-        .setTitle("🛑:"   +song.title)
+        .setTitle("🛑 : "   +song.title)
         .setURL(song.url)
         .setColor("RANDOM")
         .setAuthor(`✅ Play Now Music...`)
