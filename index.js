5be61ee1cc50245b7bad3747d85130dffd2da77e
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-18 17:03:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on(`ready`, () => {
       });
       
 
-    client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server `, {"PLAYING"});
+    client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server `, { type: "PLAYING"});
    
   
       }, (5000));
