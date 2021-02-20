30e1d7a124a37f8e88722ac797630399475e2e34
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-20 22:34:18 +0300

    Update shuffle.js

--- Music/shuffle.js
+++ Music/shuffle.js
@@ -11,7 +11,7 @@ const { PREFIX } = require(`../config.json`);
 module.exports = {
   name: "shuffle",
   aliases: ["mix"],
-  description: "(mix)Shuffles the current Queue!",
+  description: "\`(mix)Shuffles the current Queue!\`",
   cooldown: 5,
   edesc: `Type the Command, if a Queue exists to change to Order of the Queue, randomly.\nUsage: ${PREFIX}shuffle`,
 
