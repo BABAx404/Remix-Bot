151040c4401be9934e80d58422aa5d512d0dd77a
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-21 20:42:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -102,10 +102,14 @@ client.on(`message`, async (message) => {
     //define saymsg
     const saymsg = message.content.slice(Number(prefix.length) + 5)
     //define embed
-   message.react("✅").catch(console.error);
+    message.react("✅").catch(console.error);
+    return message.member
+      .send(
+        ` 🛑 **[Click here](https://discord.com/api/oauth2/authorize?client_id=792047204361175091&permissions=8&scope=bot)** to invite the bot.
+    `
+      )
     const embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setDescription("✅ **[Click here](https://discord.com/api/oauth2/authorize?client_id=792047204361175091&permissions=8&scope=bot)** to invite the bot.")
     //send the Message
     message.channel.send(embed)
   }
