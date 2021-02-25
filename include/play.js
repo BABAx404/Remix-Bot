b0e1ef15d12da15db5be8ee506bdb6af14f3680f
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-25 12:08:34 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -166,7 +166,7 @@ module.exports = {
         .setColor("RANDOM")
         .setAuthor("Play Now Music...", "https://cdn.discordapp.com/emojis/811922235018313738.gif?v=1")
         .setImage(thumb)
-        .setDescription(`**[Support](https://discord.gg/6Gv895N5hB)** | **[Invite](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=8&scope=bot)**  
+        .setDescription(`**[Support](https://discord.gg/FRZ5vAJ4Rm)** | **[Invite](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=8&scope=bot)**  
 
 • ────── <a:emoji_20:813716400408887316> ────── •`)
        
