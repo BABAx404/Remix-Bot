54a8b025c9afff05900f5f91a3b733780149de4b
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-02-25 21:17:36 +0300

    Update uptime.js

--- others/uptime.js
+++ others/uptime.js
@@ -19,8 +19,7 @@ module.exports = {
     //react with approve emoji
     message.react("<a:time:813403485902864435>");
     return message.channel.send(new MessageEmbed().setColor("RANDOM")
-        .setTitle(`<a:time:813403485902864435> 𝗨𝗽𝘁𝗶𝗺𝗲\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``))
-        .addField("Days :", `\`${days}\``, true)
+        .setTitle(`<a:time:813403485902864435> 𝗨𝗽𝘁𝗶𝗺𝗲\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
 
   }
 }
