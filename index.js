e224b3e7abccbdf7ebd028afae806c93ddd7a11b
Dark Man <77631739+dark-man99@users.noreply.github.com>
2021-03-04 14:14:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -112,6 +112,38 @@ if(message.content.startsWith(`${prefix}invite`)){
     //send the Message
     message.author.send(embed)
   } 
+   
+//An slots announcement for everyone but no one knows so fine ^w^
+  if(message.content.startsWith(`${prefix}slots`)){
+  //define the temporary Embed
+    let temEmbed = new MessageEmbed()
+    .setAuthor("Searching...", "https://cdn.discordapp.com/emojis/757632044632375386.gif?v=1").setFooter("Lyrics")
+    .setColor("RANDOM")
+    //send it and safe it in a variable
+    let result = await message.channel.send(temEmbed)
+    //try to find lyrics
+    try {
+      //use lyricsfinder
+      lyrics = await lyricsFinder(queue.songs[0].title,"");
+      //If no Lyrics define no lyrics
+      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
+    //catch any error
+    catch (error) {
+      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
+    //define lyrics Embed
+    let lyricsEmbed = new MessageEmbed()
+      .setTitle("✳️ | Lyrics")
+      .setDescription(lyrics)
+      .setColor("RANDOM")
+    //if to long make slice it
+    if (lyricsEmbed.description.length >= 2048)
+      //slice the embed description and redefine it
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+      //edit to approve
+    return result.edit(lyricsEmbed).catch(console.error);
+  }   
   
    //An help announcement for everyone but no one knows so fine ^w^
   if(message.content.startsWith(`${prefix}help`)){
