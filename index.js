3dc51f7a2b9b97f194357d341cd09bcaadf5270a
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-11 14:47:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ if(message.content.startsWith(`${prefix}invite`)){
     const embed = new Discord.MessageEmbed()
     .setDescription(`<:github:819533772311101451> Project Github
 <a:rast:813403866472251399> **[Click Here](https://github.com/slawwwy/-) to open the project.**
-      **By :**  <@637299944939585576>   
+  **By :**  <@637299944939585576>   
      
                    `)
     .setColor("RANDOM")
