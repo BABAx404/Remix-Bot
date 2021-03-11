aef68bb8d7c3b7ecb18310e63f029e613729ff1f
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-11 22:58:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.on(`ready`, () => {
      client.user.setActivity(`${PREFIX}help | Music Bot`, { type: "LISTENING"});   
            
            
-     client.user.setActivity(`Server ${client.guilds.cache.size} | Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, { type: "LISTENING"});
+     client.user.setActivity(`${client.guilds.cache.size} | Servers`, { type: "LISTENING"});
            
            
       }, (5000));    
