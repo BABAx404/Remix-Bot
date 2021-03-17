58fd181cfaa42491903b3152679594996a3cf141
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-18 02:08:23 +0300

    Update skipto.js

--- Music/skipto.js
+++ Music/skipto.js
@@ -52,7 +52,7 @@ execute(message, args) {
     //Send approve
     queue.textChannel.send(
       new MessageEmbed()
-        .setColor("#c219d8")
+        .setColor("BLACK")
         .setAuthor(`${message.author.username}#${message.author.discriminator} skipped ${args[0]} songs`, "https://cdn.discordapp.com/emojis/780373854039572501.gif?v=1")
     ).catch(console.error);
   }
