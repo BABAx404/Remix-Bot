d51533d0b6f19064504359d6fb10ac4915f7bd1b
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-20 21:19:11 +0300

    Update play.js

--- include/play.js
+++ include/play.js
@@ -147,7 +147,7 @@ module.exports = {
         .addField("Voulome ❄ :",100)
         .setURL(song.url)
         .setColor("BLACK")
-        .setAuthor(`Play Now Music...`, "https://cdn.discordapp.com/emojis/813716400408887316.gif?v=1")
+        .setAuthor(`Play Now Music...`, "https://cdn.discordapp.com/avatars/792047204361175091/c63ae917073804dcc2412c264a56c7c5.png?size=1024")
         .setThumbnail(thumb)
         .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
       var playingMessage = await queue.textChannel.send(newsong);
