e6998923a1c17d88c9e813aa35693a972df99146
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-29 15:48:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -135,45 +135,6 @@ if(message.content.startsWith(`${prefix}vote`)){
     message.author.send(embed)
   }   
    
-//An help announcement for everyone but no one knows so fine ^w^
-  if(message.content.startsWith(`${prefix}help`)){
-    //define saymsg
-    message.react("✅").catch(console.error);
-     const saymsg = message.content.slice(Number(prefix.length) + 5)
-    //define embed
-    const embed = new Discord.MessageEmbed()
-    .setThumbnail(message.author.avatarURL({dynamic: "true"}))
-    .setColor("BLACK")
-    .setImage(``)
-    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-    .setAuthor("Remix Bot ✨", "https://cdn.discordapp.com/avatars/792047204361175091/c3c061050f56e58aa1f008de8b86dd26.png?size=1024")
-    .setDescription(`
-<a:Erore:813505315534405632> **Prefix Bot**
-__** **>** **__
-◈ ━━━━━━━━ ● ━━━━━━━━ ◈
-<a:discord:813406342173818910> **User Commands**
-\`>invite\` - \`>support\` - \`>stats\` - \`avatar\`
-\`>ping\` - \`>prefix\` - \`>uptime\` - \`>embed\`
-\`>vote\` - \`>se\`
-◈ ━━━━━━━━ ● ━━━━━━━━ ◈
-<a:setting:813404135181385759> **Fun Commands**
-\`>lock(l)\` - \`>unlock(ul)\` - \`>slowmode\` - \`>clear\`
-◈ ━━━━━━━━ ● ━━━━━━━━ ◈
-<a:dark:813714900777959444> **Music Commands**
-\`>play\` - \`>skip\` - \`>skipto\`
-\`>stop\` - \`>volume\` - \`>nowplaying\`
-\`>shuffle\` - \`>search\` - \`>resume\`
-\`>remove\` - \`>queue\` - \`>filter\`
-\`>loop\` - \`>lyrics\` - \`>radio\`
-◈ ━━━━━━━━ ● ━━━━━━━━ ◈ 
-<a:links:813729922643263558> **Links**
-__**[Support](https://discord.gg/u4CbGW4qTT)**__ **-** __**[Vote](https://top.gg/bot/792047204361175091)**__ **-** __**[Invite](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=1076131920&scope=bot)**__
-`)
-
-    //send the Message
-    message.channel.send(embed)
-  }   
-   
 //An suuport announcement for everyone but no one knows so fine ^w^
   if(message.content.startsWith(`${prefix}support`)){
     //define saymsg
