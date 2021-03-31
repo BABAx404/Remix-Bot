7513bdbe070fe11fc2244330bbd8e3b08f89f466
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-31 13:22:17 +0300

    Update volume.js

--- Music/volume.js
+++ Music/volume.js
@@ -28,7 +28,7 @@ execute(message, args) {
     if (!canModifyQueue(message.member)) return;
     //define Info Embed
     const volinfoembed = new MessageEmbed()
-    .setColor("RANDOM")
+    .setColor(color)
     .setTitle(`<a:music:813460704031145986> Volume is: \`${queue.volume}%\``)
     //if no args return info embed
     if (!args[0]) return message.channel.send(volinfoembed).catch(console.error);
@@ -43,7 +43,7 @@ execute(message, args) {
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
     //define approve embed
     const volinfosetembed = new MessageEmbed()
-    .setColor("BLACK")
+    .setColor(color)
     .setTitle(`<a:music:813460704031145986> Volume changed to: \`${args[0]}%\`!`)
     //Send approve message
     return queue.textChannel.send(volinfosetembed).catch(console.error);
