a0641ac7d089778ab73919155bc1b034f8819131
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-31 13:27:56 +0300

    Update MilratoUtil.js

--- util/MilratoUtil.js
+++ util/MilratoUtil.js
@@ -9,8 +9,8 @@ module.exports = {
  async canModifyQueue(member) {
     //define the EMBED
     let resultsEmbed = new MessageEmbed()
-      .setTitle("❗️ | You must be in the Same Voice Channel as me!")
-      .setColor("RANDOM")
+      .setTitle("<a:Erore:813505315534405632> You must be in the Same Voice Channel as me!")
+      .setColor(color)
     //wenn memberchannel nicht der botchannel ist
     if (member.voice.channel !== member.guild.me.voice.channel) {
     //Send the message to the MEMBER
