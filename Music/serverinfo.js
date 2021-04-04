fffd85a2f61b8d3f99ff2643f005523cf9926442
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-04 22:07:15 +0300

    Create serverinfo.js

---
+++ Music/serverinfo.js
@@ -0,0 +1,36 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const {
+  PREFIX,
+  approveemoji,
+  denyemoji
+} = require(`../config.json`);
+const db = require('quick.db');
+
+module.exports = {
+  name: "serverinfo",
+  aliases: ["server"],
+  cooldown: 5,
+  description: "",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setColor("#FC00FF")
+    .setThumbnail(message.guild.iconURL({ dynamic: true }))
+    .setTimestamp()
+    .setAuthor(`${message.guild.name}`,message.guild.iconURL({ dynamic: true }))
+    .addField('🆔 Server ID :',`\`${message.guild.id}\``,true)
+    .addField('<a:time:813403485902864435> Created On :',message.guild.createdAt.toLocaleString())
+    .addField('<a:KING:820738504036188181> Owner Server :',`<@${message.guild.ownerID}>`,true)
+    .addField('<a:discord:813406342173818910> Members :',`\`${message.guild.memberCount}\``,true)
+    .addField('<:channel:828229201307369492> Channels :',`\`${message.guild.channels.cache.size}\``,true)
+    .addField('<a:3arz:823238395895349358> Region :',`\`${message.guild.region}\``,true)
+    .addField('<a:winner:817425780992114708> Roles :', `\`${message.guild.roles.cache.size}\``, true)
+    .addField('<a:BOOST:828272075436589077> Total Boost :',`\`${message.guild.premiumSubscriptionCount} Boost\``,true)
+    .addField('<a:Erore:813505315534405632> Emojis :',`\`${message.guild.emojis.cache.size}\``,true)
+   helpEmbed
+   message.react("✅")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
