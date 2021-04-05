f39675e02948e4f8c64be7d1f6b6d994e977c015
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-05 21:19:42 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -27,7 +27,8 @@ __** **>** **__
 <a:discord:813406342173818910> **User Commands**
 \`>invite\` - \`>support\` - \`>about\` - \`avatar\`
 \`>ping\` - \`>prefix\` - \`>uptime\` - \`>embed\`
-\`>vote\` - \`>se\` - \`>serverinfo\`
+\`>vote\` - \`>se\` - \`>serverinfo\` - \`>servericon(icon)\`
+\`>userinfo(user)\`
 ◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 <a:setting:813404135181385759> **Fun Commands**
 \`>lock(l)\` - \`>unlock(ul)\` - \`>slowmode\`  \`>clear\` - \`>ban\` - \`>unban\`
