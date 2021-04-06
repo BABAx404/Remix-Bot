709d21a82f277df53b90cd2fa8b0edf9ee77aef5
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-06 17:31:04 +0300

    Update Invite.js

--- others/Invite.js
+++ others/Invite.js
@@ -17,7 +17,8 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
    .setDescription("<a:rast:813403866472251399> **[Click here](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=1076131920&scope=bot) to invite the bot.**")
     .setColor("#FC00FF")
-   helpEmbed   message.react("<a:emoji_81:779961595656536084>")
+   helpEmbed  
+   message.react("<a:emoji_81:779961595656536084>")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
