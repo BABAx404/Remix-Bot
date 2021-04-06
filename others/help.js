e70f873a6487e8e918e0cbd94d176f536bf4a384
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-06 20:21:20 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -25,8 +25,7 @@ module.exports = {
 __** **>** **__
 ◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 <a:discord:813406342173818910> **User Commands**
-\`>invite\` - \`>invites\` - \`>support\` - \`>about\` - \`>avatar\`
-\`>ping\` - \`>prefix\` - \`>uptime\` - \`>embed\`
+\`>invite\` - \`>invites\` - \`>support\` - \`>about\` - \`>avatar\`\`>ping\` - \`>prefix\` - \`>uptime\` - \`>embed\`
 \`>vote\` - \`>se\` - \`>serverinfo\` - \`>servericon(icon)\` - \`>userinfo(user)\`
 ◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 <a:setting:813404135181385759> **Fun Commands**
