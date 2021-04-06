cc7dc6627878ad56f4312ad310f7184a74336b68
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-06 20:25:18 +0300

    Update avatar.js

--- Music/avatar.js
+++ Music/avatar.js
@@ -24,7 +24,7 @@ execute(message, args) {
    
 .setImage(member.avatarURL({size: 2048, dynamic: true, format: "png"}))
     .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
-    .addField(`Link`, `[Click Me](${member.displayAvatarURL()})`);
+    .setDescription(`[Avatar Link](${member.displayAvatarURL()})`);
     message.channel.send(Embed);
 
   }
