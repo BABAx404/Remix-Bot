dde28ced2ad7c41c0c1a931f695d10ccbe259563
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-08 17:06:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -255,36 +255,6 @@ client.on("guildDelete", guild => {
   channel.send(embed);
 });
 
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'listEmoji')) {
-  let Emojis = "";
-    let EmojisAnimated = "";
-    let EmojiCount = 0;
-    let Animated = 0;
-    let OverallEmojis = 0;
-    function Emoji(id) {
-      return client.emojis.cache.get(id).toString();
-    }
-    message.guild.emojis.cache.forEach((emoji) => {
-      OverallEmojis++;
-      if (emoji.animated) {
-        Animated++;
-        EmojisAnimated += Emoji(emoji.id);
-      } else {
-        EmojiCount++;
-        Emojis += Emoji(emoji.id);
-      }
-    });
-    let Embed = new Discord.MessageEmbed()
-      .setTitle(`Emojis in ${message.guild.name}.`)
-      .setDescription(`
-        **Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**
-      `)
-      .setColor("RANDOM");
-    message.channel.send(Embed);
-  
-}})
-
 function delay(delayInms) {
  return new Promise(resolve => {
    setTimeout(() => {
