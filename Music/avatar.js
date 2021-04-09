8431a3f00e506c67fe63cd6f76479e3bf5dd5827
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-09 17:58:40 +0300

    Update avatar.js

--- Music/avatar.js
+++ Music/avatar.js
@@ -10,9 +10,9 @@ module.exports = {
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
     const embed = new Discord.MessageEmbed()
         .setAuthor(member.user.tag, member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-        .setColor("RANDOM")
-        .setTitle(`**Link as**`)
-        .setDescription(`[png](${member.user.displayAvatarURL({format: "png", size: 1024})}) | [jpg](${member.user.displayAvatarURL({format: "jpg", size: 1024})}) | [gif](${member.user.displayAvatarURL({format: "gif", size: 1024, dynamic: true})}) | [webp](${member.user.displayAvatarURL({format: "webp", size: 1024})})`)
+        .setColor("#FC00FF")
+        .setTitle(`Link as`)
+        .setDescription(`[png](${member.user.displayAvatarURL({format: "png", size: 1024})}) | [jpg](${member.user.displayAvatarURL({format: "jpg", size: 1024})}) | [gif](${member.user.displayAvatarURL({format: "webp", size: 1024, dynamic: true})})`)
         .setImage(member.user.displayAvatarURL({ dynamic: true, size: 1024 }))
         .setFooter(`Requested By ${message.member.displayName}`, message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
     return message.channel.send(embed)
