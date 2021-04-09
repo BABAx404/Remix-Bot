c8a4bb0f26bf42f2dd71b636517a93785b3f0fde
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-09 20:18:58 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -28,8 +28,8 @@ __** **>** **__
 \`>vote\` - \`>se\` - \`>serverinfo\` - \`>servericon(icon)\`   \`>userinfo(user)\`
 ◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 <a:setting:813404135181385759> **Fun Commands**
-\`>lock(l)\` - \`>unlock(ul)\` - \`>slowmode\` - \`>ban\` - \`>unban\`
-\`>createchannel\` - \`deletechannel\` - \`>mute\` - \`>unmute\`
+\`>lock(l)\` - \`>unlock(ul)\` - \`>slowmode\` 
+\`>ban\` - \`>unban\` - \`>createchannel\` - \`deletechannel\` - \`>mute\` - \`>unmute\`
 ◈ ━━━━━━━━ ● ━━━━━━━━ ◈
 <a:dark:813714900777959444> **Music Commands**
 \`>play\` - \`>skip\` - \`>skipto\`
