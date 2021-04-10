256bdf3fd7a05dc11161eede6d0ed91d518cbb9f
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-10 23:28:11 +0300

    Create love.js

---
+++ Music/love.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "love",
+  description: "Slap somebody",
+  category: "image",
+  async execute(message, args) {
+    const data = await fetch("https://nekos.life/api/v2/img/love").then((res) =>
+      res.json()
+    );
+    const user = message.mentions.users.first() || message.author;
+    const slapped = message.author.id === user.id ? "themselfs" : user.username;
+
+    const embed = new MessageEmbed()
+      .setFooter(message.author.username)
+      .setColor("#FC00FF")
+      .setTitle(`${message.author.username} Loved ${slapped}`)
+      .setDescription(`[Click here if the image failed to load.](${data.url})`)
+      .setImage(`${data.url}`)
+      .setTimestamp();
+
+    message.channel.send({ embed });
+  },
+};
