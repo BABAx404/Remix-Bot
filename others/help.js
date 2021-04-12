c39054cc0822db8b1f356da36c6ce0d709f363d2
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-04-13 00:11:35 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,25 +20,25 @@ module.exports = {
     .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
     .setAuthor("Remix Bot ✨", "https://cdn.discordapp.com/avatars/792047204361175091/c3c061050f56e58aa1f008de8b86dd26.png?size=1024")
     .setDescription(`
-<a:discord:813406342173818910> | **User Commands**
+<a:discord:813406342173818910> **| User Commands**
 > \`invite . invites . support . about   avatar . ping . prefix . uptime   embed . cv . vote . se . serverinfo   servericon(icon) . userinfo(user)\`
 
 
 
-<a:setting:813404135181385759> | **Fun Commands**
+<a:setting:813404135181385759> **| Fun Commands**
 > \`lock(l) . unlock(ul) . slowmode . ban   unban . createchannel . deletechannel   mute . unmute\`
 
 
-<a:loading:830814256697966602> | **Gif Commands**
+<a:loading:830814256697966602> **| Gif Commands**
 > \`boy . girl\` 
 
 
-<a:dark:813714900777959444> | **Music Commands**
+<a:dark:813714900777959444> **| Music Commands**
 > \`play . skip . skipto . stop . volume   nowplaying . shuffle . search . resume   remove . queue . filter . loop   lyrics . radio\`
 
 
 
-<a:links:813729922643263558> | **Links**
+<a:links:813729922643263558> **| Links**
 __**[Support](https://discord.gg/u4CbGW4qTT)**__ **-** __**[Vote](https://top.gg/bot/792047204361175091/vote)**__ **-** __**[Invite](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=1076131920&scope=bot)**__ **-** __**[Website](https://website.remixbot9054.repl.co/)**__
 `)
    helpEmbed
