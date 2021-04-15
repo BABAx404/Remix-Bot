02990f24a8ba8f9c26c1f7cdd7d48c2f22a15826
Dark Man <77631739+DarkMan721@users.noreply.github.com>
2021-04-15 22:30:00 +0300

    Create count.js

---
+++ Music/count.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js");
+const Color = `RANDOM`;
+
+module.exports = {
+  name: "membercount",
+  category: "info",
+  async execute(message, args) {
+    const Members = message.guild.memberCount;
+    const bots = message.guild.members.cache.filter(
+      member => member.bot === true
+    ).size;
+    const humans = message.guild.members.cache.filter(
+      member => !member.user.bot
+    ).size;
+    const online = message.guild.members.cache.filter(
+      member => member.presence.status === "online"
+    ).size;
+    const offline = message.guild.members.cache.filter(
+      member => member.presence.status === "offline"
+    ).size;
+    const dnd = message.guild.members.cache.filter(
+      member => member.presence.status === "dnd"
+    ).size;
+    const idle = message.guild.members.cache.filter(
+      member => member.presence.status === "idle"
+    ).size;
+
+    const embed = new MessageEmbed()
+      .setColor(`#FC00FF`)
+      .setTitle(`Members Information`)
+      .addField(`All Members`, Members)
+      .addField(`Humans`, humans)
+      .addField(`Bots`, bots)
+      .addField(
+        `Members Status`,
+        `Online: ${online} | Do Not Disturb: ${dnd} | Idle: ${idle} | Offline: ${offline}`
+      )
+      .setTimestamp();
+
+    message.channel.send(embed);
+  }
+};
