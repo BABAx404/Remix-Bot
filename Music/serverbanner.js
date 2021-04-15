f9da9ecbd724f5beb5b58368a664ba13b24a98fb
Dark Man <77631739+DarkMan721@users.noreply.github.com>
2021-04-16 01:06:10 +0300

    Create serverbanner.js

---
+++ Music/serverbanner.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+
+
+
+module.exports = {
+    name: 'banner',
+   aliases: ["avatar-server", "icon"],
+   cooldown: 5,
+    description: "Displays the icon of the server.",
+
+    execute(message, args) {
+        const { guild } = message
+        let icon = guild.iconURL({size: 4096, dynamic: true})
+
+        const servericonEmbed = new Discord.MessageEmbed()
+        .setAuthor(`${message.guild.name}`,message.guild.iconURL({ dynamic: true }))
+        .setDescription(`[Banner URL](${message.guild.bannerURL}?size=2048)`)
+        .setImage(message.guild.bannerURL() + "?size=2048")
+        .setColor("#FC00FF")
+        .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+
+        message.channel.send(servericonEmbed)
+    }
+}
