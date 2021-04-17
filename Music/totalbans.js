f6ea1eb6729b15b62e47b1841cf7b9d94b2b6a96
Dark Man <77631739+DarkMan721@users.noreply.github.com>
2021-04-17 04:34:41 +0300

    Add files via upload

---
+++ Music/totalbans.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "bans",
+    cooldown: 10,
+    category: "extra",
+    async execute(message, args) {
+   message.guild.fetchBans().then(bans => {
+            message.channel.send(`${bans.size} Total Member Bans`)
+        })
+
+    }
+}
