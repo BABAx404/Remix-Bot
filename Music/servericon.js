bd68fd6638c98ce1537eebd242f9a4d5bb47ee52
Dark Man <77631739+DarkMan3852@users.noreply.github.com>
2021-04-19 22:36:39 +0300

    Update servericon.js

--- Music/servericon.js
+++ Music/servericon.js
@@ -13,7 +13,7 @@ module.exports = {
         let icon = guild.iconURL({size: 4096, dynamic: true})
 
         const servericonEmbed = new Discord.MessageEmbed()
-        .setAuthor(`${message.guild.name}`,message.guild.iconURL({ dynamic: true }))
+        .setAuthor(`${message.guild.name}`, message.guild.iconURL({ dynamic: true }))
         .setImage(icon)
         .setColor("#FC00FF")
         .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
