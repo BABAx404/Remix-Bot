a0be008efd09a4109b25573272d95b0e58cb05fb
Dark Man <77631739+DarkMan3852@users.noreply.github.com>
2021-04-20 20:11:52 +0300

    Update totalbans.js

--- Music/totalbans.js
+++ Music/totalbans.js
@@ -13,6 +13,8 @@ async execute(message, args) {
                 .setTitle(`**Ban's**`)
                 .setColor("#FC00FF")
                 .setDescription(`**Banned User**: ${bans.size}`)
+                .setAuthor(`${message.guild.name}`,message.guild.iconURL({ dynamic: true }))
+                .setThumbnail(message.guild.iconURL({ dynamic: true }))
                 .setFooter("Status")
                 .setTimestamp()
             message.channel.send(bansembed)
