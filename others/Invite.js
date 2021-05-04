65555d755a842f981b9a2f97fd14589c7664bf49
Dark Man <77631739+DarkMan1223@users.noreply.github.com>
2021-05-05 01:45:11 +0300

    Update Invite.js

--- others/Invite.js
+++ others/Invite.js
@@ -1,25 +1,45 @@
-const { Client, Collection, MessageEmbed } = require(`discord.js`);
-const {
-  PREFIX,
-  approveemoji,
-  denyemoji
-} = require(`../config.json`);
-const db = require('quick.db');
-
+const Discord = require("discord.js")
 module.exports = {
   name: "invite",
-  aliases: ["Invite"],
-  cooldown: 1.5,
-  description: "",
-  execute(message) {
-    let commands = message.client.commands.array();
+  aliases: ["inv"],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  cooldown: 3000,
+  async execute(bot, message, args) {
+      
+      if(message.author.bot || !message.guild) return message.reply(new Discord.MessageEmbed().setColor("BLUE").setDescription(`This command for serverOnly`));
+         var embed = new Discord.MessageEmbed()
+         .setColor("BLUE")
+         .setDescription(`Send link here 1️⃣\nSend in DM 2️⃣`)
+         message.channel.send(embed).then(async message1 => {
+    await  message1.react("1️⃣")
+        message1.react("2️⃣")
+    const filter = (reaction, user) =>
+        reaction.emoji.name === '1️⃣' && user.id === message.author.id;
+        const filter1 = (reaction, user) =>
+        reaction.emoji.name === '2️⃣' && user.id === message.author.id;
+ const yes = message1.createReactionCollector(filter, {time: 300000})
+        const no = message1.createReactionCollector(filter1, {time: 300000})
+        
+yes.on('collect', bbb => {
+ 
+  message1.delete()
+var embed = new Discord.MessageEmbed()
+ .setColor("BLUE")
+.addField("Security",("[Click here](https://discord.com/api/oauth2/authorize?client_id=743869029638471802&permissions=8&scope=bot)"))
+message.channel.send(embed)
 
-    let helpEmbed = new MessageEmbed()
-   .setDescription("<a:rast:813403866472251399> **[Click here](https://discord.com/oauth2/authorize?client_id=792047204361175091&permissions=1076131920&scope=bot) to invite the bot.**")
-    .setColor("#FC00FF")
-   helpEmbed  
-   message.react("<a:emoji_81:779961595656536084>")
-    return message.channel.send(helpEmbed).catch(console.error);
-
+})
+no.on('collect', nnn => {
+  message1.delete()
+  var embed10 = new Discord.MessageEmbed()
+  .setColor("BLUE")
+.addField("Security",("[Click here](https://discord.com/api/oauth2/authorize?client_id=743869029638471802&permissions=8&scope=bot)"))
+message.channel.send(new Discord.MessageEmbed().setColor("BLUE").setDescription(`Check your DM`));
+message.author.send(embed10)
+})
+         })     
   }
-};
+}
