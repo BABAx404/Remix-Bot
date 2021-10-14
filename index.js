3796e48331e9b34a0b50e75494e01fbb87bf7191
hardijabar0 <79927633+hardijabar0@users.noreply.github.com>
2021-10-15 01:30:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODkxMDA3MjA4MDM5NTQyODE0.YU4FIA.x1htSwRTBoLpmJ_L2SC0KqZijHM');
+client.login('ODcyNTc1NDQ0MjE2NTc4MTQ5.YQr3NQ.8klMF1jwlvMmHxdhgWimk_eyFi0');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
